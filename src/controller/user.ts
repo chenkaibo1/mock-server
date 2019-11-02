4402913545a8d34891eed79a3770fa82bd5026df
chenkaibo <chenkaibo@58ganji-corp.com>
2019-11-02 18:19:21 +0800

    添加用户头像

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -4,12 +4,22 @@ import * as md5 from 'md5'
 import passportLocal from '../common/possport'
 import * as jwt from 'jsonwebtoken'
 import config from '../config'
+import { merge, random } from 'lodash'
+const gravatar = [
+  '//img.souche.com/20161230/png/58f22ad636a0f33bad8762688f78d425.png',
+  '//img.souche.com/20161230/png/6cdcda90c2f86ba1f45393cf5b26e324.png',
+  '//img.souche.com/20161230/png/f9d10bb683d940dd14dc1b1344e89568.png',
+  '//img.souche.com/20161230/png/8bb4f0fd45ed6ae26533eadd85f0f7ea.png',
+  '//img.souche.com/20161230/png/0795744371fd5869af6cab796bdacb95.png',
+  '//img.souche.com/20161230/png/bc836261fbb654dda6b653e428014279.png',
+  '//img.souche.com/20161230/png/fd9f8aecab317e177655049a49b64d02.png'
+]
 // 用户登录
 export const login = async (ctx: ParameterizedContext, next: any) => {
   try {
-    const { username, password } = ctx.request.body
+    const { username } = ctx.request.body
     const exists = await User.findOne({ username })
-    if (!exists) await User.create({ username, password: md5(md5(password)) })
+    if (!exists) await User.create(merge(ctx.request.body, { headImg: gravatar[random(0, gravatar.length - 1)] }))
     return passportLocal.authenticate('local', async (err, user, info) => {
       if (err) {
         ctx.body = ctx.resp.fail({ message: err })
@@ -18,7 +28,7 @@ export const login = async (ctx: ParameterizedContext, next: any) => {
         ctx.body = ctx.resp.success({
           message: '登陆成功',
           data: {
-            user: { id: user._id, username: user.username },
+            user: { id: user._id, username: user.username, headImg: user.headImg },
             token: jwt.sign({ id: user._id, email: user.email, username: user.username }, config.secret, {
               expiresIn: '7d'
             })
--- src/tools/initData.ts
+++ src/tools/initData.ts
@@ -5,7 +5,11 @@ async function initUser() {
   try {
     const user = await User.findOne({ username: config.admin.username })
     if (!user) {
-      User.create({ username: config.admin.username, password: md5(md5(config.admin.password)) })
+      User.create({
+        username: config.admin.username,
+        password: md5(md5(config.admin.password)),
+        headImg: '//img.souche.com/20161230/png/fd9f8aecab317e177655049a49b64d02.png'
+      })
     }
   } catch (error) {
     console.log(error)
